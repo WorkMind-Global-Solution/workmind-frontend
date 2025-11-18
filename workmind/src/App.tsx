56c8554fd6fa07d7d3c40897e960d8d55de6d18b
Gustavo-Casimiro <bardy.gustavo@gmail.com>
2025-11-18 12:48:15 -0300

    feat: Cria o layout principal usando Outlet, Header e Footer

--- workmind/src/App.tsx
+++ workmind/src/App.tsx
@@ -1,7 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Header from './components/layout/Header'; 
+import Footer from './components/layout/Footer';
+
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
-      <h1 className="text-3xl font-bold">WorkMind</h1>
+    <div className="flex flex-col min-h-screen">
+      <Header /> 
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <Footer /> 
     </div>
   );
 }
