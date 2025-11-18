0e2f3a329dddd43d10e72762bcdbfe28cfe150c8
henriquepacheco07 <rm562086>
2025-11-18 12:28:41 -0300

    feat: Instala e configura o React Router DOM para roteamento de páginas

--- workmind/src/types/workmind.ts
+++ workmind/src/types/workmind.ts
@@ -1,28 +1,29 @@
-// Interface para dados do Aluno
+// Interface para dados do Alunos
 export interface Integrante {
-  rm: string;         
-  nome: string;       
-  turma: string;      
-  fotoUrl: string;    
-  githubLink: string; 
-  linkedinLink: string; 
+  rm: string;         
+  nome: string;
+  turma: string;      
+  fotoUrl: string;    
+  githubLink: string; 
+  linkedinLink: string; 
 }
 
 export type StatusMissao = 'pendente' | 'em_progresso' | 'completa';
 
 // Interface para dados da Missão
 export interface Missao {
-  id: number;         
-  titulo: string;
-  status: StatusMissao;
-  xp: number;
+  id: number;         
+  titulo: string;
+  status: StatusMissao;
+  xp: number;
 }
 
 export type PerfilMentorado = Integrante & {
-  // Combina dados básicos de Integrante com dados do Perfil da IA
-  objetivos: string[];
-  plano: 'basico' | 'pro' | 'premium'; // Union Type
-  ultimaAtividade: Date;
+  // Combina dados básicos de Integrante com dados do Perfil 
+  objetivos: string[];
+  plano: 'basico' | 'pro' | 'premium'; 
+  ultimaAtividade: Date;
 };
 
-export type Theme = "light" | "dark";
+// Tipo para o estado do tema 
+export type Theme = "light" | "dark"; 
