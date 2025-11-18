4a7e17a96460c488deff78fd458936f69511d4d0
henriquepacheco07 <rm562086>
2025-11-18 12:25:00 -0300

    feat: Adiciona interface Integrante com campos básicos

---
+++ workmind/src/types/workmind.ts
@@ -0,0 +1,9 @@
+// Interface para dados do Aluno
+export interface Integrante {
+  rm: string;         
+  nome: string;       
+  turma: string;      
+  fotoUrl: string;    
+  githubLink: string; 
+  linkedinLink: string; 
+}
