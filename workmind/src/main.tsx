a1c9cbfc14906b68a0bedad2732cc565f03ef4ae
SiqueiraMartins <rafael.siqueira.martins@outlook.com>
2025-11-18 13:39:38 -0300

    feat: Adicionando ThemeProvider no main.

--- workmind/src/main.tsx
+++ workmind/src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { AppRoutes } from './routes'; 
+import { ThemeProvider } from './contexts/ThemeContext'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppRoutes /> 
+    <ThemeProvider> 
+      <AppRoutes /> 
+    </ThemeProvider>
   </React.StrictMode>,
 );
